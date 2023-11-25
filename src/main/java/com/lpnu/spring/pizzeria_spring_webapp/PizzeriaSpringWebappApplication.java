7183174db7c324c017add88937fe3ae198d771f8
deltaonlyone <deltaonlyone@gmail.com>
2023-11-25 10:58:25 +0200

    Project created

---
+++ src/main/java/com/lpnu/spring/pizzeria_spring_webapp/PizzeriaSpringWebappApplication.java
@@ -0,0 +1,13 @@
+package com.lpnu.spring.pizzeria_spring_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaSpringWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaSpringWebappApplication.class, args);
+    }
+
+}
